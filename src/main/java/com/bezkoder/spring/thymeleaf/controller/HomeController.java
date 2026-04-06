43b9185b92817951d0dbb978385034a497c38ee2
Sujal <sujalasaini2468@gmail.com>
2026-04-06 12:24:10 +0530

    new

---
+++ src/main/java/com/bezkoder/spring/thymeleaf/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/tutorials";
+    }
+}
